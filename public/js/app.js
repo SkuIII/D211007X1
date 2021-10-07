e3d6baef7c131895514820cb3e3b5e28519b7e49
Oscar <oscargus0247@gmail.com>
2021-10-07 13:03:41 +0200

    yep

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,28 @@
 'use strict';
 
-console.log('I am also alive!');
+let dataURL = '../data/exercise1.json';
+let req = new XMLHttpRequest();
+req.open('GET', dataURL); 
+req.responseType='json'; 
+req.send(); 
+
+console.log('data received');
+
+req.onload = () => {
+    const data = req.response;
+    const header = data['headline'];
+    const intro = data['intro'];
+
+    const h1 = document.createElement('h1');
+    const h1Text = document.createTextNode(header);
+
+    h1.appendChild(h1Text);
+
+    const p = document.createElement('p');
+    const pText = document.createTextNode(intro);
+    
+    p.appendChild(pText);
+
+    document.getElementById('Exercise1').appendChild(h1);
+    document.getElementById('Exercise1').appendChild(p);
+};
